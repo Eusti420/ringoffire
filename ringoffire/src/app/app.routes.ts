bc07a9b75096518df5d564cdf155afcb85f4c370
Eusti420 <163431211+Eusti420@users.noreply.github.com>
2024-09-05 14:36:24 +0200

    start screen added

--- ringoffire/src/app/app.routes.ts
+++ ringoffire/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { StartingscreenComponent } from './starting-screen/startingscreen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: StartingscreenComponent }
+
+];
---
+++ ringoffire/src/app/starting-screen/startingscreen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-startingscreen',
+  standalone: true,
+  imports: [],
+  templateUrl: './startingscreen.component.html',
+  styleUrl: './startingscreen.component.scss'
+})
+export class StartingscreenComponent {
+
+}
