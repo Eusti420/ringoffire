291efb141c4bdf78d7aaed0e6eef593874717429
Eusti420 <163431211+Eusti420@users.noreply.github.com>
2024-09-06 18:54:19 +0200

    newGame function

--- ringoffire/src/app/app.routes.ts
+++ ringoffire/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { StartingscreenComponent } from './starting-screen/startingscreen.component';
+import { GameComponent } from './game/game.component';
 
 export const routes: Routes = [
-    { path: '', component: StartingscreenComponent }
+    { path: '', component: StartingscreenComponent },
+    { path: 'game', component: GameComponent }
 
 ];
--- ringoffire/src/app/starting-screen/startingscreen.component.ts
+++ ringoffire/src/app/starting-screen/startingscreen.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-startingscreen',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './startingscreen.component.html',
   styleUrl: './startingscreen.component.scss'
 })
+
+
 export class StartingscreenComponent {
 
+  constructor(private router: Router) {
+
+  }
+
+
+  newGame() {
+    this.router.navigateByUrl('/game');
+  }
+  
+
 }
+
+
