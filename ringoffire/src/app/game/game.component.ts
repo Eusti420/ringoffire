f1fdb0b52a420d81d82020088cb66883f6db4329
Eusti420 <163431211+Eusti420@users.noreply.github.com>
2024-09-07 14:20:59 +0200

    game class added

--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
+
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
 
+  pickCardAnimation = false;
+  game: Game;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+    
+  }
 }
