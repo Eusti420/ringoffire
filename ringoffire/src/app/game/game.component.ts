3f93be0bc0d4087750e079d5f57e8478d4fb1cb7
Eusti420 <163431211+Eusti420@users.noreply.github.com>
2024-09-07 15:32:14 +0200

    take card function

--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -11,9 +11,9 @@ import { Game } from '../../models/game';
   styleUrl: './game.component.scss'
 })
 export class GameComponent implements OnInit {
-
   pickCardAnimation = false;
-  game: Game;
+  game!: Game;
+  currentCard: string  = '';
 
   constructor() {
 
@@ -21,14 +21,21 @@ export class GameComponent implements OnInit {
 
   ngOnInit(): void {
     this.newGame();
-  }
+    
+  };
 
   newGame() {
     this.game = new Game();
-  }
+  };
 
   takeCard() {
-    this.pickCardAnimation = true;
-    
-  }
+    if(!this.pickCardAnimation) {
+      this.pickCardAnimation = true;
+      this.currentCard = this.game.stack.pop();
+      this.game.playedCards.push(this.currentCard)
+        setTimeout(() => {
+          this.pickCardAnimation = false;
+        }, 1500)
+    };
+  };
 }
